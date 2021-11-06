36c3d0ebb7223993f5b79d04f30e1011c427482b
carlos <caaz160782@gmail.com>
2021-11-05 22:26:48 -0600

    se creo ruta para busqueda * texto

---
+++ routes/busquedaPostRouter.js
@@ -0,0 +1,24 @@
+const express=  require("express");
+const router = express.Router()
+const post = require("../usecases/posts");
+
+router.get("/",async (request, response, next)=>{
+      const {titlePost} = request.query
+      try{    
+        const postInfo = await post.find(titlePost)
+        response.json({
+                ok:true,
+                message:"Done",
+                listPost:{ postInfo },
+            })
+     }
+    catch (error){
+    next(error)
+    response.status(404).json({
+        ok:false,
+        message:"Post not found"
+   })
+  }
+}) 
+
+module.exports = router;
